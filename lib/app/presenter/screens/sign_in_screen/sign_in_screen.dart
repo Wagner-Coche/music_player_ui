75bb0d1930e71abda9298980f3e6718b313e20c6
Wagner-Coche <wagnercoche@gmail.com>
2022-09-24 17:31:23 +0100

    feature: colors utils and background in sign_in

--- lib/app/presenter/screens/sign_in_screen/sign_in_screen.dart
+++ lib/app/presenter/screens/sign_in_screen/sign_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/app/presenter/utils/colors_utils.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({Key? key}) : super(key: key);
@@ -16,6 +17,28 @@ class SignInScreenWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: ColorsUtils.colorsUtils.darkBlue),
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [ColorsUtils.colorsUtils.darkBlue, ColorsUtils.colorsUtils.black12],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                )
+              ),
+            ),
+          ),
+        ]
+      ),
+    );
   }
 }
+
+//000A13
+//959EA9;
---
+++ lib/app/presenter/utils/colors_utils.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorsUtils {
+  static ColorsUtils colorsUtils = ColorsUtils();
+  
+  Color darkBlue = const Color(0xff001D3A);
+  Color black12 = Colors.black12;
+}
